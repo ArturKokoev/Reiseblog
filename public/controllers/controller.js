aca157cc210b4f19765b8c0e38fc5bd0f94f06da
Artur Kokoev <artur94@live.de>
2018-05-14 13:51:46 +0200

    v4 done

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,27 +2,26 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
-person1= {
-name: 'Etka',
-email: 'Etkinator@email.com',
-number: '(777) 121-4321'
+
+    var refresh = function(){
+  $http.get('/reiseblog').then (function(response) {
+    console.log("I got the data I requested");
+    $scope.reiseblog = response.data;
+  console.log(response.data);
+  });
 };
 
-person2 = {
-name: 'Cuma',
-email: 'cuma93@live.de',
-number: '0177 5885523'
-};
+refresh();
 
-person3 = {
-name: 'Aman',
-email: 'Playboy@live.de',
-number: '0124 0234233'
+$scope.addContact = function(){
+	console.log($scope.contact);
+	$http.post('/reiseblog', $scope.contact).then(function(response){
+		console.log(response);
+		refresh();
 
-};
+	});
+}
 
-var contactlist = [person1, person2, person3];
-$scope.contactlist = contactlist;
 
-    }]);﻿
+ }]);﻿
 
--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs ('reiseblog', ['reiseblog']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
+app.get('/reiseblog', function (req, res){
+	console.log("I received a GET request")
+
+
+
+db.reiseblog.find(function (err, docs){
+	console.log(docs);
+	res.json(docs);
+});
+
+});
+
+app.post('/reiseblog', function (req, res){
+	console.log(req.body);
+	db.reiseblog.insert(req.body, function (err, doc){
+
+		res.json(doc);
+
+	});
+});
 
 app.listen(3000);
-console.log("Server running");
+console.log("Yes ! Server running on Port 3000");
 
+   
