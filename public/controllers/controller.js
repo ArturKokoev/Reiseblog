c8b3d35fe60f6d6d13c3d24094a54dc29acd4043
Artur Kokoev <artur94@live.de>
2018-05-15 13:59:44 +0200

    contactlist app done

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,27 +1,56 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
+  $scope.ShowAddContact = true;
 
 
-    var refresh = function(){
-  $http.get('/reiseblog').then (function(response) {
-    console.log("I got the data I requested");
-    $scope.reiseblog = response.data;
-  console.log(response.data);
-  });
-};
+  var refresh = function() {
+    $http.get('/reiseblogdata').then(function(response) {
+      console.log("I got the data I requested");
+      $scope.reiseblogdata = response.data;
 
-refresh();
+    });
+  };
 
-$scope.addContact = function(){
-	console.log($scope.contact);
-	$http.post('/reiseblog', $scope.contact).then(function(response){
-		console.log(response);
-		refresh();
+  refresh();
 
-	});
-}
+  $scope.addContact = function() {
+    console.log($scope.contact);
+    $http.post('/reiseblogdata', $scope.contact).then(function(response) {
+      console.log(response);
+      $scope.contact = {};
+      refresh();
+
+    });
+  };
+
+  $scope.remove = function(id) {
+    console.log(id);
+    $http.delete('/reiseblogdata/' + id).then(function(response) {
+      refresh();
+    });
+  };
+
+  $scope.edit = function(id) {
+    console.log(id);
+    $http.get('/reiseblogdata/' + id).then(function(response) {
+      $scope.contact = response.data;
+      $scope.ShowUpdate = true;
+      $scope.ShowAddContact = false;
+
+    });
+  };
+
+  $scope.update = function() {
+    console.log($scope.contact._id);
+    $http.put('/reiseblogdata/' + $scope.contact._id, $scope.contact).then(function(response) {
+      refresh();
+      $scope.ShowUpdate = false;
+      $scope.ShowAddContact = true;
+      $scope.contact = {};
+
+    })
+  };
 
 
- }]);﻿
 
+}]);﻿
--- server.js
+++ server.js
@@ -1,34 +1,57 @@
+// MEAN Stack RESTful API Tutorial - Contact List App
+
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs ('reiseblog', ['reiseblog']);
+var db = mongojs('reiseblogdata', ['reiseblogdata']);
 var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/reiseblog', function (req, res){
-	console.log("I received a GET request")
+app.get('/reiseblogdata', function (req, res) {
+  console.log('I received a GET request');
 
-
-
-db.reiseblog.find(function (err, docs){
-	console.log(docs);
-	res.json(docs);
+  db.reiseblogdata.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
 });
 
+app.post('/reiseblogdata', function (req, res) {
+  console.log(req.body);
+  db.reiseblogdata.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
 });
 
-app.post('/reiseblog', function (req, res){
-	console.log(req.body);
-	db.reiseblog.insert(req.body, function (err, doc){
+app.delete('/reiseblogdata/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.reiseblogdata.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
 
-		res.json(doc);
+app.get('/reiseblogdata/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.reiseblogdata.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
 
-	});
+app.put('/reiseblogdata/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.reiseblogdata.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
 });
 
 app.listen(3000);
-console.log("Yes ! Server running on Port 3000");
-
-   
+console.log("Server running on port 3000");
